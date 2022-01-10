47a5816a59ab27d8de1e2b2843aa3b29fc955ac2
nitrotap <kartikinpublic@gmail.com>
2022-01-10 02:58:40 -0700

    setting up first question

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,46 @@
 var timerEl = document.querySelector("#timer");
+var time = 90;
+var titleEl = document.querySelector("#main-title");
+var subTitleEl = document.querySelector("#sub-title");
+var startButtonEl = document.querySelector("#start-button");
 
-time = 90;
-var timer = function () {
-  if (time > 0) {
-    time--;
-    timerEl.innerHTML = time;
-  } else {
-    clearInterval(timer);
-  }
+var timer = function (seconds) {
+    if (time > 0) {
+        time--;
+        timerEl.innerHTML = time;
+    } else {
+        clearInterval(timer);
+    }
 };
 
 var countdown = function () {
-  var intervalID = setInterval(timer, 1000);
+    var intervalID = setInterval(timer, 1000);
 };
 
-countdown();
+var startScreen = function() {
+    titleEl.innerHTML = "<h1>Coding Quiz Challenge<h1>";
+    subTitleEl.innerHTML = "<h3> A simple coding quiz to test your coding knowledge!</h3>";
+    startButtonEl.innerHTML = "<h2>Start Quiz</h2>";
+    startButtonEl.addEventListener("click", question1);
+}
+
+var question1 = function() {
+    console.log("question 1");
+    titleEl.innerHTML = "<h1>Question 1: Question Here!<h1>";
+    subTitleEl.remove();
+    
+    var answerBox = document.createElement("div");
+    answerBox.className = "answers";
+    var answer1 = document.createElement("button");
+    answer1.textContent = "the answer is here";
+
+    
+}
+
+
+var quiz = function() {
+    question1();
+}
+
+
+startScreen();
